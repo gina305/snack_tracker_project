dd033ff571a27264c4517b277d4e4d3603b4d26d
gina305 <napier.gina@gmail.com>
2022-08-26 05:34:09 -0400

    updated code

--- snacks_tracker_project/snacks/models.py
+++ snacks_tracker_project/snacks/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# Create your models here.
+# Create a snack model
 class Snack(models.Model):
 
 	# Define fields
 	name = models.CharField(max_length=64)
-	#purchaser= models.ForeignKey('self', on_delete=models.CASCADE)#Define one-to-mny relationship betweeen purchaser
-	# and snacks.
+	purchaser= models.ForeignKey(get_user_model(),on_delete=models.CASCADE)#Define one-to-mny relationship betweeen
+	# purchaser
+	# and snacks. (model to relate to, what to do when the purchaser is delweted in the snack model
 	description = models.TextField(max_length=30)
 
--- snacks_tracker_project/snacks/views.py
+++ snacks_tracker_project/snacks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+# ListVeiw is used tio list based on a template
 # Create your views here.
+class SnackListView(ListView):
+	template_name =  "snack_list.html"
+	model = # associate a a model to this vie. Define
+
+
+	1. Dfine Models in models.py
+
+
